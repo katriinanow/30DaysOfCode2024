fcb6d173acc29d57e2b1c84300ea2a2a84c8e93b
katriinanow <62251467+katriinanow@users.noreply.github.com>
2024-10-06 22:07:15 +0200

    day 0 challenge

---
+++ src/main/java/org/example/Day0.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Day0 {
+
+    public static void day0() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Hello, World. \n" + input);
+        scanner.close();
+    }
+}
